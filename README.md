fc4b2d252af2fc3b4066c18ed6c3367a9596d2b6
Geunje Lee <glee@mz.co.kr>
2021-12-08 13:49:23 +0900

    refactor: new action tool

--- README.md
+++ README.md
@@ -1,47 +1,74 @@
-# How to use
+# actions
+중앙 집중형 github action workflow 관리 도구
+
+## 개요
+spaceone MSA에 공통적으로 사용되는 github action workflow를 관리하고, 각 repository에 배포한다.<br>
+배포된 workflow는 각 repository에서 Trigger될 때 action의 deploy workflow를 호출하여 지속적으로 최신 상태를 유지할 수 있다.
+
+<img src = "https://user-images.githubusercontent.com/19552819/145136525-608930c0-8ea4-44d0-a5db-a127a495fee1.png" width="50%" height="50%">
+
+
+## 사용 방법
+github action을 trigger하여 배포할 수 있다.
+- input
+  - [required]group : 동일한 workflow를 사용하는 group을 지정.
+  - [option]repository : 특정 repository만을 지정하고 싶을때 사용.
+![스크린샷 2021-12-08 오전 11 20 35](https://user-images.githubusercontent.com/19552819/145137059-251ecb7a-6279-471f-bb85-298c9ccf580e.png)
+
+### group에 대해
+spaceone-dev의 repository는 동일한 github action workflow를 사용하는 경우가 있다.<br>
+때문에 이러한 동일한 workflow를 사용하는 Repository를 group으로 묶고 deploy시에 이것을 활용한다.
+
+|group|repository|
+|---|---|
+|backend|spaceone-dev/inventory,identity,config,notification....|
+|plugin|spaceone-dev/plugin-aws...,plugin-google....,plugin-azure....   |
+|console|spaceone-dev/console|
+|console-api|spaceone-dev/console-api|
+
+## 설정
+
+### 배포 대상 repository 설정
+conf.yaml에 정의한다.<br>
+새로운 repository가 추가되거나, 관리 대상에서 벗어난 repository가 있다면, 이곳에서 추가/삭제를 하면된다.<br>
 
 ```
-$ python src/app.py
-```
-
-### ENVIRONMENT = PRD
-
-using `conf.yaml`
-
-### ENVIRONMENT = DEV
-
-using `dev-conf.yaml`
-
-### Structure of the configuretaion file
-
-```
-# an example for conf.yaml
-
-# Types of repositories
-rTypes: 
-  - name: backend
-    # repositories which are included in a repository type
-    repositories:
-      - name: config
-        url: github.com/spaceone-dev/config
-      - name: identity
-        url: github.com/spaceone-dev/identity
-      ...
-  - name: console
-    repositories:
-      - name: console
-        url: github.com/spaceone-dev/console
+rTypes:                            
+  backend:                        # workflow group
+    - name: spaceone-dev/config   # repository name
+    - name: spaceone-dev/identity
+  console:
+    - name: spaceone-dev/console
+  plugin:
+    - name: spaceone-dev/plugin-azure-cloud-service-inven-collector
 ```
 
 
+### workflow 추가
+각 group의 workflows에 추가하고 싶은 workflow 파일을 추가해둔다.<br>
+추후 deploy workflow가 동작하면서, 새롭게 추가된 파일을 지정 repository에 추가해준다.
+```
+├── README.md
+├── backend
+│   └── workflows     ★ 이곳
+│       └── sample.yaml
+├── console
+│   └── workflows     ★ 이곳
+│       └── sample.yaml
+├── plugin
+│   └── workflows     ★ 이곳
+│       └── sample.yaml  
+├── conf.yaml
+├── requirements.txt
+├── src
+│   ├── main.py
+│   └── module
+└── venv
+    ├── bin
+    ├── lib
+    └── pyvenv.cfg
+```
 
-## How to develop
-
-### For development
-
-* **Option 1. Push to dev**
-  * push to `dev` branch. It detects the differences between the latest 2 commits. Then it uses `dev-conf.yaml` which includes repositories for test and development
-* **Option 2. Merge `dev` into `master`**
-  * Use **squash** because it refers to two latest commits. If not, it wouldn't be able to fetch two latest desired commits. (i.e - you might refer to multiple garbage commits in `dev` branch.)
-* **Options 3. Just push to master**
-
+### 새로운 group 추가
+새로운 group이 추가된다면, group 이름의 directory를 생성 후 workflows/<..>.yaml을 생성한다.<br>
+[update중...]
---
+++ src/main.py
@@ -0,0 +1,96 @@
+from github import Github
+from github.GithubException import UnknownObjectException,GithubException
+from module import argparse as ap
+import logging, os, sys, yaml
+logging.basicConfig(level=logging.INFO)
+args = ap.parse_args()
+
+def main():
+    token = os.getenv('PAT_TOKEN',None)
+    if not token:
+        logging.error('PAT_TOKEN does not set')
+        sys.exit(1)
+    client = Github(token)
+
+    config = get_config()
+    rTypes = config.get('rTypes',None)
+    if not rTypes:
+        logging.error('config file is empty')
+        sys.exit(1)
+
+    group = args.group
+<<<<<<< HEAD
+    specified_repo = args.repo
+
+    workflows = get_workflows(group)
+    for repo in rTypes[group]:
+        if specified_repo and repo['name'] != specified_repo:
+=======
+    specific_repo = args.repo
+
+    workflows = get_workflows(group)
+    for repo in rTypes[group]:
+        if specific_repo and repo['name'] != specific_repo:
+>>>>>>> 87c15b0 (refactor: Add simple concept)
+            continue
+        repo_name = repo['name']
+        update_file_in_repository(client, repo_name, workflows)
+
+def create_new_file_in_repository(client, repo_name, workflows):
+    repo = client.get_repo(repo_name)
+
+    try:
+        for workflow in workflows:
+            for path,content in workflow.items():
+                ret = repo.create_file(path=path, message="[CI] Deploy CI", content=content, branch="master")
+                logging.info(f'file has been created to {repo_name} : {ret}')
+    except GithubException as e:
+        logging.info(f'failed to file creation (Maybe file already exists or Resource not found): {e}')
+    except Exception as e:
+        raise e
+
+def update_file_in_repository(client, repo_name, workflows):
+    repo = client.get_repo(repo_name)
+
+    try:
+        for workflow in workflows:
+            for path,content in workflow.items():
+                contents = repo.get_contents(path,ref="master")
+                ret = repo.update_file(path=contents.path,message="[CI] Update CI",content=content,sha=contents.sha,branch="master")
+                logging.info(f'file has been updated in {repo_name} : {ret}')
+    except UnknownObjectException as e:
+        logging.warning(f'failed to update {repo_name}: {e}')
+        logging.warning("Maybe the file doesn't exist. Attempt to create a file")
+        create_new_file_in_repository(client, repo_name, workflows)
+    except Exception as e:
+        raise e
+
+def get_config():
+    try:
+        with open('./conf.yaml', "r") as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+        return config
+    except FileNotFoundError as e:
+        logging.error(e)
+        sys.exit(1)
+    except yaml.YAMLError as e:
+        logging.error(f"Invalid yaml format! {e}")
+        sys.exit(1)
+
+def get_workflows(group):
+    workflow_path = f'./{group}/workflows'
+    workflow_names = os.listdir(workflow_path)
+
+    github_actions = []
+    for workflow_name in workflow_names:
+        github_action_set = {}
+        with open(f'{workflow_path}/{workflow_name}','r') as f:
+            body = f.read()
+        path = f'.github/workflows/{workflow_name}'
+        github_action_set[path] = body
+        github_actions.append(github_action_set)
+
+    return github_actions
+
+if __name__ == "__main__":
+    main()
