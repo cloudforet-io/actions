6238f663d6a37ad26af4bc4121c4657da22f71ab
G.J.LEE <glee@mz.co.kr>
2023-05-16 14:26:41 +0900

    fix: fix concurrent future for all dest
    Signed-off-by: G.J.LEE <glee@mz.co.kr>

--- src/service/actions_service.py
+++ src/service/actions_service.py
@@ -14,8 +14,14 @@ class ActionsService:
     def deploy(self, org, dest, type):
         # TODO: Need to test concurrent.futures is working correctly
         if dest == 'all':
-            # self.deploy_all(org)
-            pass
+            # TODO: Support single topic
+            # Currently, only double topics are supported.
+            # see workflow_mgr -> list_workflow_directory_name()
+
+            destinations = self.actions_mgr.find_all_destinations(org)
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                futures = [executor.submit(self._deploy_in_concurrent, destination) for destination in destinations]
+                concurrent.futures.wait(futures)
         else:
             destinations = self.actions_mgr.list_destinations(org, dest, type)
 
@@ -25,15 +31,7 @@ class ActionsService:
                 for workflow in workflows:
                     self.github_mgr.commit(destination, workflow)
 
-    def deploy_all(self, org):
-        # TODO: Support single topic
-        # Currently, only double topics are supported.
-        # see workflow_mgr -> list_workflow_directory_name()
-        destinations = self.actions_mgr.find_all_destinations(org)
-
-        for destination in destinations:
-            workflows = self.actions_mgr.list_workflows(destination)
-
-            with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-                for workflow in workflows:
-                    executor.submit(self.github_mgr.commit, destination, workflow)
+    def _deploy_in_concurrent(self, destination):
+        workflows = self.actions_mgr.list_workflows(destination)
+        for workflow in workflows:
+            self.github_mgr.commit(destination, workflow)
