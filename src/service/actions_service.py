dcdeaf1c262b9eabd13554cd6b957c0592329f8e
G.J.LEE <glee@mz.co.kr>
2023-05-24 10:18:45 +0900

    fix: merge and rename _deploy_in_concurrent

--- src/service/actions_service.py
+++ src/service/actions_service.py
@@ -1,37 +1,30 @@
 import concurrent.futures
 
 from manager.github_manager import GithubManager
-from manager.workflow_manager import WorkflowManager
 from manager.actions_manager import ActionsManager
 
 
 class ActionsService:
     def __init__(self):
         self.github_mgr = GithubManager()
-        self.workflow_mgr = WorkflowManager()
         self.actions_mgr = ActionsManager()
 
     def deploy(self, org, dest, type):
-        # TODO: Need to test concurrent.futures is working correctly
         if dest == 'all':
             # TODO: Support single topic
             # Currently, only double topics are supported.
-            # see workflow_mgr -> list_workflow_directory_name()
 
             destinations = self.actions_mgr.find_all_destinations(org)
             with concurrent.futures.ThreadPoolExecutor() as executor:
-                futures = [executor.submit(self._deploy_in_concurrent, destination) for destination in destinations]
+                futures = [executor.submit(self._commit, destination) for destination in destinations]
                 concurrent.futures.wait(futures)
         else:
             destinations = self.actions_mgr.list_destinations(org, dest, type)
 
             for destination in destinations:
-                workflows = self.actions_mgr.list_workflows(destination)
+                self._commit(destination)
 
-                for workflow in workflows:
-                    self.github_mgr.commit(destination, workflow)
-
-    def _deploy_in_concurrent(self, destination):
+    def _commit(self, destination):
         workflows = self.actions_mgr.list_workflows(destination)
         for workflow in workflows:
             self.github_mgr.commit(destination, workflow)
